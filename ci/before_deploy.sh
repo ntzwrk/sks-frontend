b55eee10e129168b9b1b1ca0796a0bc7d2677dd6
Valentin Brandl <vbrandl@riseup.net>
2018-06-19 22:53:34 +0200

    Create ZIP archive

--- ci/before_deploy.sh
+++ ci/before_deploy.sh
@@ -13,6 +13,7 @@ main() {
   cd $stage
   tar czf $src/sks-frontend-$TRAVIS_TAG.tar.gz *
   tar cjf $src/sks-frontend-$TRAVIS_TAG.tar.bz2 *
+  zip -r $src/sks-frontend-$TRAVIS_TAG.zip *
   cd $src
 
   rm -rf $stage
